c799954640284a433ce663f75597c96f9786c089
sbcoder1 <133533441+sbcoder1@users.noreply.github.com>
2024-10-26 13:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def home():
     return render_template('index.html')  #  'index.html' 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
